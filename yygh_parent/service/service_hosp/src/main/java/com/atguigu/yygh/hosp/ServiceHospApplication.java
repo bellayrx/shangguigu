36d0659e707ebd4c5f8b8ab18263bff40a3140f8
bellayrx <bellayrx@gmail.com>
2021-09-18 21:35:04 +0800

    feat(重新進行配置)

--- yygh_parent/service/service_hosp/src/main/java/com/atguigu/yygh/hosp/ServiceHospApplication.java
+++ yygh_parent/service/service_hosp/src/main/java/com/atguigu/yygh/hosp/ServiceHospApplication.java
@@ -2,7 +2,9 @@ package com.atguigu.yygh.hosp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
+@ComponentScan(basePackages = "com.atguigu")
 @SpringBootApplication
 public class ServiceHospApplication {
     public static void main(String[] args) {
--- yygh_parent/service/service_hosp/src/main/java/com/atguigu/yygh/hosp/controller/HospitalSetController.java
+++ yygh_parent/service/service_hosp/src/main/java/com/atguigu/yygh/hosp/controller/HospitalSetController.java
@@ -1,13 +1,61 @@
 package com.atguigu.yygh.hosp.controller;
 
+import com.atguigu.yygh.common.result.Result;
 import com.atguigu.yygh.hosp.service.impl.HospitalSetServiceImpl;
+import com.atguigu.yygh.model.hosp.HospitalSet;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.apache.ibatis.annotations.Delete;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+@Api(tags = "医院管理")
 @RestController
 @RequestMapping("/admin/hosp/hospitalSet")
 public class HospitalSetController {
     @Autowired
     private HospitalSetServiceImpl service;
+
+    @ApiOperation(value = "获取列表")
+    @GetMapping("/findall")
+    public  Result getAllHospSet(){
+        //調用service
+        List<HospitalSet> list = service.list();
+
+        return Result.ok(list);
+    }
+    //删除
+    @ApiOperation(value = "逻辑删除")
+    @DeleteMapping("{id}")
+    public Result removeHospSet(@PathVariable Long id){
+        boolean b = service.removeById(id);
+        if(b){
+            return Result.ok(b);
+        }else {
+            return Result.fail();
+        }
+    }
+
+    /*
+    * 條件查詢帶分頁
+    * */
+
+
+    /*
+    *
+    * 添加醫院設置
+    * */
+
+
+
+    /*
+    * 修改醫院設置
+    *根據ID獲取
+    * */
+
+
+    /*
+    * 批量刪除
+    * */
 }
